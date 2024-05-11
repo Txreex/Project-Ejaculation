7c1717273c4aad6596117e8b98013327667ef1d3
YJuDeAd <robinrashi@gmail.com>
2024-05-11 16:17:11 +0530

    test

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const path = require('path');
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Test',
-        width: 700,
-        height: 500
+        width: 800,
+        height: 600
     });
 
     mainWindow.loadFile(path.join(__dirname, './render/index.html'));
