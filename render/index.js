28cf4860eeff20b1df529f931a82979c1ebb1868
YJuDeAd <robinrashi@gmail.com>
2024-06-02 01:27:15 +0530

    progress

--- render/index.js
+++ render/index.js
@@ -2,6 +2,12 @@ document.getElementById("bell").onclick = function(){
     console.log("Search")
 }
 
+// this is for opening the menu in smol screens (it just works dont ask me)
+document.getElementById("menu-toggle").onclick = function () {
+    var navLinks = document.querySelector(".nav__links");
+    navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+};
+
 var video = document.getElementById("bak");
 video.muted = true;
 video.volume = 0.3; // Adjust volume between 0 and 1
