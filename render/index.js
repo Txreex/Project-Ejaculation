71dce54d131492e6236d56f9429a0046b2751c80
YJuDeAd <robinrashi@gmail.com>
2024-06-01 22:56:37 +0530

    test

---
+++ render/index.js
@@ -0,0 +1,7 @@
+document.getElementById("bell").onclick = function(){
+    console.log("Search")
+}
+
+var video = document.getElementById("bak");
+video.muted = true;
+video.volume = 0.3; // Adjust volume between 0 and 1
