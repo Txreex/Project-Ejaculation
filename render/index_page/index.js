4b55e3dda850331889e7023f76a29d5cec6785e9
YJuDeAd <robinrashi@gmail.com>
2024-07-03 23:25:29 +0530

    commented

--- render/index_page/index.js
+++ render/index_page/index.js
@@ -1,8 +1,9 @@
-import { fetchAppList } from '../../backend/steamApi.js';
+import { fetchAppList } from '../../backend/steamApi.js'; //function for retriving artworks
 import { changeImage } from '../../backend/changeImage.js'
 
 let description, logoUrl, vid_link;
 
+//default game window
 window.onload = async function() {
   const appDetails = await fetchAppList ("Counter-Strike 2");
   if (appDetails) {
@@ -15,13 +16,7 @@ window.onload = async function() {
   }
 };
 
-document.querySelector('.search-btn').addEventListener('click', searchApp);
-document.querySelector('.search-input').addEventListener('keyup', function(event) {
-  if (event.key === 'Enter') {
-    searchApp();
-  }
-});
-
+//function to define working of search
 async function searchApp() {
   const query = document.querySelector('.search-input').value;
   const appDetails = await fetchAppList(query);
@@ -35,27 +30,40 @@ async function searchApp() {
   }
 }
 
+document.querySelector('.search-btn').addEventListener('click', searchApp);
+document.querySelector('.search-input').addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    searchApp();
+  }
+});
+
+//hamburger menu when screen is small
 document.getElementById("menu-toggle").onclick = function () {
   var navLinks = document.querySelector(".nav__links");
   navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
 };
 
+//sends to download page
 document.getElementById("download").onclick = function(){
   window.location.href = "../download_page/download.html"
 }
 
+//sends to library page
 document.getElementById("lib").onclick = function(){
   window.location.href = "../library_page/library.html"
 }
 
+//sends to default home page
 document.getElementById("home").onclick = function(){
   window.location.href = "index.html"
 }
 
+//sends to more-info page
 document.getElementById("more-info").onclick = function(){
   window.location.href = "../more-info/more-info.html"
 }
 
+//functioning of the volume button
 document.getElementById("vol_button").onclick = function() {
     changeImage("vol_button","../Assets/Icons/mute.png","../Assets/Icons/unmute.png")
     let video = document.getElementById("Vid");
